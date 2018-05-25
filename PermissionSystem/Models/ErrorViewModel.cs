ad266ee19c1b2a7b701a9d41952994e96882f8ea
yangxd <yangxd@BIM-PC16-DEV>
2018-05-25 11:36:41 +0800

    添加项目文件。

---
+++ PermissionSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PermissionSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
